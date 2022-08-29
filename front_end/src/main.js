01e02865289c0fc8d930ca4763372da4ce767343
somenzz <somenzz@163.com>
2022-08-29 10:29:01 +0800

    first commit

---
+++ front_end/src/main.js
@@ -0,0 +1,12 @@
+import {
+    createApp
+} from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './assets/main.css'
+import './mock'
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
